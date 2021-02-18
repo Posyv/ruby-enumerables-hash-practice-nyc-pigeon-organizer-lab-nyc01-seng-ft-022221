f400eea725d6e0617740a6bc774eaf248aa28dc3
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 23:29:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), accumulatorarr|
+  the_end = data.each_with_object({}) do |(key, value), accumulatorarr|
     binding.pry
     value.each do |insidekey, names|
       names.each do |lastname|
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
 accumulatorarr[lastname][key].push(insidekey)
         end
     end
-    accumulatorarr
+    #accumulatorarr
   end
   binding.pry
   # write your code here!
