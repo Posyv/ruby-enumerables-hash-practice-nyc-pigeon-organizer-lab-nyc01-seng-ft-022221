339157c25fd37810fa91160dbde87966fd438b06
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-18 23:22:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,16 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
+  data.each_with_object({}) do |(key, value), accumulatorarr|
+    binding.pry
+    value.each do |insidekey, names|
+      names.each do |lastname|
+        if !accumulatorarr[lastname]
+          accumulatorarr[lastname] = {}
+        end
+        end
+    end
+    accumulatorarr
+  end
   binding.pry
   # write your code here!
 end
